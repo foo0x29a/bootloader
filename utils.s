d59e84b42d6f45ebd83207ad7100772e0f6bfe7e
foo0x29a <thiago.nobayashi@gmail.com>
2019-11-30 23:12:43 +0900

    Fix indentation

--- utils.s
+++ utils.s
@@ -1,17 +1,17 @@
 .equ TTY_OUT, 0x10
  
- print:
-   push %bp 
-   mov %sp, %bp 
-   mov 4(%bp), %si 
-   mov $0x0e, %ah 
-   _print:
-     lodsb
-     cmp $0, %al 
-     je _done
-     int $TTY_OUT
-     jmp _print
-   _done:
-     mov %bp, %sp 
-     pop %bp 
-     ret
+print:
+  push %bp
+  mov %sp, %bp
+  mov 4(%bp), %si
+  mov $0x0e, %ah
+  _print:
+    lodsb
+    cmp $0, %al
+    je _done
+    int $TTY_OUT
+    jmp _print
+  _done:
+    mov %bp, %sp
+    pop %bp
+    ret
