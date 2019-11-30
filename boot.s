c83097416b8acc675ba16cf849b2de54784d5578
foo0x29a <thiago.nobayashi@gmail.com>
2019-11-30 23:11:00 +0900

    Move print function to a separate file

--- boot.s
+++ boot.s
@@ -1,33 +1,19 @@
 .code16
 .globl _start, start
 
- start:
- _start:
-   push $real_mode_message
-   call print
-   addw $2, %sp
+start:
+  _start:
+    push $real_mode_message
+    call print
+    addw $2, %sp
 
-   hlt
+    hlt
 
- print:
-   push %bp
-   mov %sp, %bp
-   mov 4(%bp), %si
-   mov $0x0e, %ah
-   _print:
-     lodsb
-     cmp $0, %al
-     je _done
-     int $0x10
-     jmp _print
-   _done:
-     mov %bp, %sp
-     pop %bp
-     ret
+.include "utils.s"
 
- real_mode_message:
-   .asciz "Entering 16 bits Real Mode"
+real_mode_message:
+  .asciz "Entering 16 bits Real Mode..."
 
- .fill 510-(.-start), 1, 0
- .byte 0x55
- .byte 0xaa
+.fill 510-(.-start), 1, 0
+.byte 0x55
+.byte 0xaa
